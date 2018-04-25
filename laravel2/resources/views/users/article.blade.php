4418b3f58a9755a05f05c57366180fbf42ed45fa
together0 <1433925240@qq.com>
2018-04-25 13:55:47 +0800

    forth laravel

---
+++ laravel2/resources/views/users/article.blade.php
@@ -0,0 +1,13 @@
+@extends('users.articleModel')
+
+@section('blog',$blog)
+
+@section('action',$action)
+
+@section('nickName',$nickName)
+
+@section('artContent','')
+
+@section('artTitle','')
+
+@section('artId','')
---
+++ laravel2/routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*页面的显示*/
+Route::get('/','users\HomeController@show');
+
+/*登录页面的显示*/
+Route::get('/login','users\LoginController@login');
+
+/*注册页面的显示*/
+Route::get('/register','users\RegisterController@register');
+
+/*获取登录信息*/
+Route::post('/formdata/login','users\LoginController@loginData');
+
+/*获取注册信息*/
+Route::post('/formdata/register','users\RegisterController@registerData');
+
+/*注册成功页面*/
+Route::get('/islogin','users\HomeController@islogin');
+
+/*登录成功页面*/
+Route::get('/isregister','users\HomeController@isregister');
+
+/*个人博客页面*/
+Route::get('/blog/{id}','users\BlogController@perBlog');
+
+/*发布文章页面*/
+Route::get('/blog/write/show/{id}','users\BlogController@show');
+
+/*获取发布博客的信息*/
+Route::post('/blog/write/getData/{id}','users\BlogController@getData');
+
+/*发布照片页面*/
+Route::get('/blog/photo/show/{id}','users\BlogController@showPhoto');
+
+/*获取发布照片的信息*/
+Route::post('/blog/photo/getData/{id}','users\BlogController@getPhoto');
+
+/*修改博客页面*/
+Route::get('/blog/revise/{id}','users\BlogController@revise');
+
+/*获取修改博客页面的信息*/
+Route::post('/blog/revise/getData/{id}','users\BlogController@getRevise');
+
+/*删除博客*/
+Route::get('/blog/delete/{art_id}','users\BlogController@delete');
